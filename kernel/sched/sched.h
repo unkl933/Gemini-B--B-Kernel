4c7f3dce453287ae8addadd530b9b855ac77a9d3
Juri Lelli <juri.lelli@arm.com>
2015-06-26 12:14:23 +0100

    FROMLIST: sched/{core,fair}: trigger OPP change request on fork()
    Patch "sched/fair: add triggers for OPP change requests" introduced OPP
    change triggers for enqueue_task_fair(), but the trigger was operating only
    for wakeups. Fact is that it makes sense to consider wakeup_new also (i.e.,
    fork()), as we don't know anything about a newly created task and thus we
    most certainly want to jump to max OPP to not harm performance too much.
    
    However, it is not currently possible (or at least it wasn't evident to me
    how to do so :/) to tell new wakeups from other (non wakeup) operations.
    
    This patch introduces an additional flag in sched.h that is only set at
    fork() time and it is then consumed in enqueue_task_fair() for our purpose.
    
    BUG=chrome-os-partner:44828
    TEST=Boot kernel on Oak.
    
    Change-Id: I0e485e7a2e6386f276eefa7920b2fc34f7877c22
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    Signed-off-by: Steve Muckle <smuckle@linaro.org>
    (am from https://patchwork.kernel.org/patch/7805001/)
    Signed-off-by: Punit Agrawal <punit.agrawal@arm.com>
    
    Conflicts:
        kernel/sched/sched.h

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1138,6 +1138,8 @@ static const u32 prio_to_wmult[40] = {
 #else
 #define ENQUEUE_WAKING		0x00
 #endif
+#define ENQUEUE_MIGRATING	0x40
+#define ENQUEUE_WAKEUP_NEW	0x50
 
 #define RETRY_TASK		((void *)-1UL)
 
